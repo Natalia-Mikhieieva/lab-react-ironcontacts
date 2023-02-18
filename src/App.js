f1b3fe2ca2ea611edbb28891cb72b5fdb442c9a4
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2023-02-18 16:43:47 +0100

    Iteration 1 & 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import "./App.css";
-import contacts from "./contacts.json";
+import contactJson from "./contacts.json";
+import React, { useState } from 'react';
 
 function App() {
+  const [contacts, setContacts] = useState(contactJson.slice(0,5));
+  
+  function addRandom() {
+    const newContact = (Math.floor(Math.random() * contacts.length))
+
+    if(!contacts.includes(newContact)){
+      setContacts([newContact,...contacts])
+    }
+  }
+  function sortContacts(){
+    const sortedContacts = [...contacts]
+
+    sortedContacts.sort((a,b) => {
+      if(a.name < b.name){
+        return -1
+      } else if(b.name < a.name){
+        return 1
+      }
+      return 0
+    })
+    setContacts(sortedContacts)
+  }
+
+  function sortByRating(){
+    const sortedContacts = [...contacts]
+
+    sortedContacts.sort((a,b) => {
+      if(a.popularity < b.popularity){
+        return 1
+      } else if(b.popularity < a.popularity){
+        return -1
+      }
+      return 0
+    })
+    setContacts(sortedContacts)
+  }
   return (
     <div className="App">
+    <button onClick={addRandom}>Add a contact</button>
+    <button onClick={sortContacts}>Sort contacts by name</button>
+    <button onClick={sortByRating}>Sort contacts by popularity</button>
       <table>
         <thead>
           <tr>
             <th>Picture </th>
             <th>Name </th>
             <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
           </tr>
         </thead>
         <tbody>
@@ -21,6 +63,8 @@ function App() {
                 </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity}</td>
+                <td>{contact.wonOscar}</td>
+                <td>{contact.wonEmmy}</td>
               </tr>
             );
           })}
